739b717de0412600d78ba1afc593d913469e1d63
Damith Anushka <damithanushkam016@gmail.com>
2023-09-02 13:12:11 +0530

    Add customer form and completed suturing

---
+++ stock-management-system/src/main/java/org/example/launcher/Launcher.java
@@ -0,0 +1,26 @@
+package org.example.launcher;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.util.Objects;
+
+public class Launcher extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent parent = FXMLLoader.load(Objects.requireNonNull(this.getClass().getResource("/view/main_form.fxml")));
+        Scene scene = new Scene(parent);
+        stage.setTitle("My Form");
+        stage.centerOnScreen();
+        stage.setScene(scene);
+        stage.show();
+    }
+}
